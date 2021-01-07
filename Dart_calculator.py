692545dc2380d58cccfb9a976c5b5c4d9fcf63f0
AdrianStecko <adrian.stecko@gmail.com>
2021-01-07 20:42:20 +0100

    Add second version

--- Dart_calculator.py
+++ Dart_calculator.py
@@ -3,8 +3,13 @@
 game_type = None
 
 while game_type != "301" and game_type != "501":
-    game_type = input("Wybierz typ gry 301 lub 501: ")
+    game_type = input("Choose type of the game: ")
     if game_type != "301" and game_type != "501":
-        print("Wybierz poprawny typ gry!!!!!!!!!!")
+        print("Choose correct type of the game!!!")
 
-print("Good answer", game_type, "to Twój wybór")
+print("Good choice", game_type, "is your type of game")
+
+player_1 = input("What is first player name? ")
+player_2 = input("What is second player name? ")
+
+print("So lets begin the", game_type)
